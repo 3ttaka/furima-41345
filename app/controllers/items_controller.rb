47ff6aeb3237bc3aaee6a5b6fd375b929e18985f
3ttaka <s3ttaka0723@gmail.com>
2024-07-28 15:38:41 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    return unless current_user.id != @item.user.id
+
+    redirect_to root_path
   end
 
   def update
