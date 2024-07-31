66b9e02d031642a9e65235f80f20fd4b42d10dc1
3ttaka <s3ttaka0723@gmail.com>
2024-07-31 21:20:06 +0900

    修正

--- spec/factories/order_deliveries.rb
+++ spec/factories/order_deliveries.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     address                  { Faker::Address.street_address }
     building_name            { Faker::Address.secondary_address }
     phone_number             { Faker::Number.number(digits: [10, 11].sample).to_s }
-    user_id                  { 1 }
-    item_id                  { 1 }
     token                    { 'tok_abcdefghijk00000000000000000' }
   end
 end
